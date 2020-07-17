ec3733257aa5506bbd5106f827834ab428efb5a5
guy-black <67166723+guy-black@users.noreply.github.com>
2020-07-17 10:12:19 -0500

    woo

---
+++ precache-manifest.b6ef05a980cf4ff48428005b1b5b68d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd95db06b5e8c64ea31acad7a66fbba",
+    "url": "/Fortunes/index.html"
+  },
+  {
+    "revision": "804690cd2376a2779676",
+    "url": "/Fortunes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34ff4fce0ef5cdc01da2",
+    "url": "/Fortunes/static/js/2.bc9584d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fortunes/static/js/2.bc9584d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804690cd2376a2779676",
+    "url": "/Fortunes/static/js/main.ae17fb87.chunk.js"
+  },
+  {
+    "revision": "dafd2fb252280e9589f5",
+    "url": "/Fortunes/static/js/runtime-main.bb2f3581.js"
+  }
+]);
