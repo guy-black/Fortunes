656a5d10873fe299355ebd3a4da73bf4f14788e7
guy-black <67166723+guy-black@users.noreply.github.com>
2020-07-17 10:25:44 -0500

    woooo

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fortunes/precache-manifest.b6ef05a980cf4ff48428005b1b5b68d4.js"
+  "/Fortunes/precache-manifest.6b881287e3f4a409346dadfdb6104a7f.js"
 );
 
 self.addEventListener('message', (event) => {
